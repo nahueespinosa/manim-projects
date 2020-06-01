2643ba790cb765eb1acfeb1af78fc9ff4a31ab91
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-06-01 19:43:46 -0300

    Update animation

--- 00_signal_parameters.py
+++ 00_signal_parameters.py
@@ -11,24 +11,32 @@ class FrecuencyPeriod(GraphScene):
         "function_color" : RED,
         "axes_color" : GREEN,
         "x_labeled_nums" : range(0, 5, 1),
-        "x_axis_label": "$t[s]$",
+        "x_axis_label": "$t[ms]$",
         "y_axis_label": "$V[V]$",
     }
 
     def construct(self):
         self.setup_axes(animate=True)
         func_graph = self.get_graph(self.func_to_graph, self.function_color)
-        vert_line_1 = self.get_vertical_line_to_graph(1, func_graph, color=YELLOW)
-        vert_line_2 = self.get_vertical_line_to_graph(2, func_graph, color=YELLOW)
-        graph_label = self.get_graph_label(func_graph, label="\\cos(x)")
-        two_pi = TexMobject("T = 1 segundo")
-        label_coord = self.input_to_graph_point(1.5, func_graph)
-        two_pi.next_to(label_coord, UP)
+
+        vert_line_1 = Line(self.coords_to_point(1, -1.5), self.coords_to_point(1, 1.5), color=YELLOW)
+        vert_line_2 = Line(self.coords_to_point(2, -1.5), self.coords_to_point(2, 1.5), color=YELLOW)
+
+        period_measurement = TexMobject("T = 1 ms")
+        period_measurement.set_x(self.coords_to_point(1, 1.7)[0], LEFT+DOWN)
+        period_measurement.set_y(self.coords_to_point(1, 1.7)[1], LEFT+DOWN)
 
         self.play(ShowCreation(func_graph))
-        self.play(ShowCreation(graph_label))
-        self.play(ShowCreation(vert_line_1))
-        self.play(ShowCreation(vert_line_2), ShowCreation(two_pi))
+        self.play(ShowCreation(vert_line_1), ShowCreation(vert_line_2))
+        self.play(ShowCreation(period_measurement))
+
+        self.play(
+            ApplyMethod(vert_line_1.move_to, self.coords_to_point(2.75, 0)),
+            ApplyMethod(vert_line_2.move_to, self.coords_to_point(3.75, 0)),
+            ApplyMethod(period_measurement.set_x, self.coords_to_point(2.75, 0)[0], LEFT+UP)
+        )
+
+        self.wait(1)
 
     def func_to_graph(self, x):
         return np.cos(2*math.pi*x)
